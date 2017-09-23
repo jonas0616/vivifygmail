ea3daa20f1678926afeb2b3b3c220a103c5158af
Ping-Che Hsiao <pingchehsiao@gmail.com>
2017-09-23 18:17:56 +0800

    Add vivify.user.js

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # vivifygmail
-Keep your POP3 account in Gmail with latest infomation
+Keep your POP3 account in Gmail up to date
+
+### How does it work
+As long as you open a Gmail window/tab in your browser, VivifyGmail
+automatically send a refresh request to Gmail every mintute.
+
+[VivifyGmail on Greasy Fork](https://greasyfork.org/en/scripts/33433-vivifygmail)
---
+++ vivify.user.js
@@ -0,0 +1,67 @@
+// ==UserScript==
+// @name         VivifyGmail
+// @namespace    https://github.com/jonas0616/vivifygmail
+// @version      0.1
+// @description  Keep your POP3 account in Gmail up to date
+// @author       jonas0616
+// @grant        none
+// @include      https://mail.google.com/*
+// @require      http://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js
+// @license      Apache License 2.0
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    Promise.resolve()
+
+    .then(() => {
+        return new Promise((resolve) => {
+            let id = setInterval(() => {
+				if (window.GM_ACTION_TOKEN !== undefined &&
+					window.GLOBALS !== undefined
+				) {
+					clearInterval(id);
+					resolve();
+				}
+			}, 5000);
+        });
+    })
+
+    .then((data) => {
+        let url = window.location.href.split('#')[0];
+		let at = window.GM_ACTION_TOKEN;
+		let ik = window.GLOBALS[9];
+
+        // check act is valid
+		let refreshUrl = `${url}?ik=${ik}&&at=${at}&view=up&act=par&rt=j`;
+
+		// repeat by one minute
+		window.setInterval(() => {
+			refresh(refreshUrl);
+		}, 60000);
+    });
+
+	function refresh(refreshUrl) {
+		$.ajax({
+			url: refreshUrl,
+			method: 'POST',
+			xhr: () => {
+				let xhr = $.ajaxSettings.xhr();
+				let setRequestHeader = (name, value) => {
+					// ignore X-Requested-With
+					if (name === 'X-Requested-With') {
+						return;
+					}
+					setRequestHeader.call(this, name, value);
+				};
+				return xhr;
+			}
+		})
+
+		.catch((err) => {
+			console.log(err);
+		});
+	}
+
+})();
