53004b58678cd7d5b7d7f2e12a556458275eeb0a
Ping-Che Hsiao <pingchehsiao@gmail.com>
2017-09-23 20:33:04 +0800

    Refine coding style
    1. Replace windows-formatted newline to unix one
    2. Fix mixed indent
    3. Simplify setInterval usage

--- vivify.user.js
+++ vivify.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         VivifyGmail
 // @namespace    https://github.com/jonas0616/vivifygmail
-// @version      0.1
+// @version      0.1.1
 // @description  Keep your POP3 account in Gmail up to date
 // @author       jonas0616
 // @grant        none
@@ -18,50 +18,45 @@
     .then(() => {
         return new Promise((resolve) => {
             let id = setInterval(() => {
-				if (window.GM_ACTION_TOKEN !== undefined &&
-					window.GLOBALS !== undefined
-				) {
-					clearInterval(id);
-					resolve();
-				}
-			}, 5000);
+                if (window.GM_ACTION_TOKEN !== undefined &&
+                    window.GLOBALS !== undefined
+                ) {
+                    clearInterval(id);
+                    resolve();
+                }
+            }, 5000);
         });
     })
 
     .then((data) => {
         let url = window.location.href.split('#')[0];
-		let at = window.GM_ACTION_TOKEN;
-		let ik = window.GLOBALS[9];
+        let at = window.GM_ACTION_TOKEN;
+        let ik = window.GLOBALS[9];
+        let refreshUrl = `${url}?ik=${ik}&&at=${at}&view=up&act=par&rt=j`;
 
-        // check act is valid
-		let refreshUrl = `${url}?ik=${ik}&&at=${at}&view=up&act=par&rt=j`;
-
-		// repeat by one minute
-		window.setInterval(() => {
-			refresh(refreshUrl);
-		}, 60000);
+        // repeat by one minute
+        window.setInterval(refresh, 60000, refreshUrl);
     });
 
-	function refresh(refreshUrl) {
-		$.ajax({
-			url: refreshUrl,
-			method: 'POST',
-			xhr: () => {
-				let xhr = $.ajaxSettings.xhr();
-				let setRequestHeader = (name, value) => {
-					// ignore X-Requested-With
-					if (name === 'X-Requested-With') {
-						return;
-					}
-					setRequestHeader.call(this, name, value);
-				};
-				return xhr;
-			}
-		})
+    function refresh(refreshUrl) {
+        $.ajax({
+            url: refreshUrl,
+            method: 'POST',
+            xhr: () => {
+                let xhr = $.ajaxSettings.xhr();
+                let setRequestHeader = (name, value) => {
+                    // ignore X-Requested-With
+                    if (name === 'X-Requested-With') {
+                        return;
+                    }
+                    setRequestHeader.call(this, name, value);
+                };
+                return xhr;
+            }
+        })
 
-		.catch((err) => {
-			console.log(err);
-		});
-	}
-
+        .catch((err) => {
+            console.log(err);
+        });
+    }
 })();
